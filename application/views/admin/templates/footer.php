e9436dda7c350cbdc9a2e2fa5d87d72d42e75ee5
dwielok <dwieloknuraini773@gmail.com>
2022-12-27 08:10:41 +0700

    adding an templates

---
+++ application/views/admin/templates/footer.php
@@ -0,0 +1,25 @@
+</div>
+<!-- ============================================================== -->
+<!-- End Wrapper -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="<?= base_url('assets/libs/jquery/dist/jquery.min.js') ?>"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="<?= base_url('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('assets/dist/js/app-style-switcher.js') ?>"></script>
+<!--Wave Effects -->
+<script src="<?= base_url('assets/dist/js/waves.js') ?>"></script>
+<!--Menu sidebar -->
+<script src="<?= base_url('assets/dist/js/sidebarmenu.js') ?>"></script>
+<!--Custom JavaScript -->
+<script src="<?= base_url('assets/dist/js/custom.js') ?>"></script>
+<!--This page JavaScript -->
+<!--chartis chart-->
+<script src="<?= base_url('assets/libs/chartist/dist/chartist.min.js') ?>"></script>
+<script src="<?= base_url('assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') ?>"></script>
+<script src="<?= base_url('assets/dist/js/pages/dashboards/dashboard1.js') ?>"></script>
+</body>
+
+</html>

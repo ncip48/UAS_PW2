ee570e59ccf7e3effac05ac84c61e85ef14c559d
ncip48 <mbahcip00@gmail.com>
2022-12-29 10:23:46 +0700

    change email dynamic

--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -654,7 +654,7 @@ $user = $this->db->get_where('tb_user', ['id' => $user_data['id']], 1, 0)->row()
 														<path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
 														<polyline points="22,6 12,13 2,6"></polyline>
 													</svg>
-													info@Flexy.com
+													<?= $user->email ?>
 												</p>
 											</div>
 										</div>
